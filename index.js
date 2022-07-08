3984468bf53c13a67a6ce97742b6107777948a32
dolly-mane <dollymane121@gmail.com>
2022-07-08 17:09:44 +0530

    all routes modified with MongoDB Operation

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const mongoose=require('mongoose');
+require('dotenv').config()
 const app = express();
+
+const Task = require("./model/Task");
 
 app.use(express.json())
 
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 
 //to check server is running or not
@@ -18,122 +21,114 @@ app.get('/health',(req,res)=>{
 //temp database
 let tasks = [];
 
-//create a task
-app.post('/tasks',(req,res)=>{
+mongoose.connect(process.env.MONGODB_URI ,()=>{
 
-    const task = {
-        'id':req.body.id,
-        'title':req.body.title,
-        'description':req.body.description,
-        'priority':req.body.priority,
-        'emoji':req.body.emoji,
-    }
-
-    tasks.push(task);
-
-    res.json({
-        'status':'success',
-        'message': 'task added successfully',
-        'data':task
-    })
-
-
+    console.log("Connected to MongoDB Database....")
 
 })
+
+
+
+//create a task
+app.post('/tasks',async(req,res)=>{
+
+    const task = new Task({
+        id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        priority: req.body.priority,
+        emoji: req.body.emoji,
+    })
+
+    const savedTask = await task.save();
+
+    res.json({
+               'status': 'success',
+               'data': savedTask
+           })
+
+ });
+
+
+
 
 //To read all tasks
-app.get('/tasks' , (req,res) => {
+app.get('/tasks' , async(req,res) => {
 
+    const allTasks = await Task.find();
     res.json({
         'status':'success',
-        'data':tasks
-    })
+        'data': allTasks
+    });
     
-})
+});
 
 //to read specific task
-app.post('/get_task',(req,res) =>{
+app.post('/get_task',async(req,res) =>{
+
     const id = req.body.id;
 
-    let resultTask;
+   const specificTask = await Task.findOne({id: id});
 
-    tasks.map((task) => {
-       if(task === id){
-        resultTask = task;
-       }
-    })
 
     res.json({
         'status':'success',
-        'data': resultTask
+        'data': specificTask
     })
 
 })
 
 
 //to delete all tasks
-app.post('/delete_tasks',(req,res) =>{
-    tasks=[]
+app.post('/delete_tasks',async(req,res) =>{
+    
+    const result = await Task.deleteMany();
     res.json({
         'status':'success',
         'message':'deleted all tasks',
-        'data':tasks
+        'data':result
     })
 })
 
 //to delete specific task
-app.post('/delete_task',(req,res) =>{
+app.post('/delete_task',async(req,res) =>{
    
     const id = req.body.id;
+    
+    const result = await Task.deleteOne({id: id});
 
-    let index = -1;
-
-    tasks.map((task,i) =>{
-        if(id === task.id)
-        {
-            index = i;
-        }
-    })
-    tasks.splice(index, 1)
 
     res.json({
         'status':'success',
         'message':`successfully deleted task by ${id}`,
-        'data': tasks
+        'data': result
     })
 })
 
 //to update tasks
-app.post('/update_task',(req,res) =>{
+app.post('/update_task',async(req,res) =>{
     const id = req.body.id;
     const title = req.body.title;
     const description = req.body.description;
     const priority = req.body.priority;
     const emoji = req.body.emoji;
 
-    let index = -1;
-    tasks.map((task, i) =>{
-        if(id === task.id)
-        {
-            index=i;
-        }
+    const updateResult = await Task.updateOne({id: id},{
+        $set: {
+             title: title,
+             description: description,
+             priority: priority,
+             emoji: emoji,
+          }
     })
-
-    tasks[index] =  {
-        id: id,
-        title: title,
-        description: description,
-        priority: priority,
-        emoji: emoji
-    }
 
     res.json({
         'status':'success',
         'message': 'successfully task updated',
-        'data': tasks
-    })
+        'data': updateResult
+    });
 
-})
+});
 
 app.listen(PORT,()=>{
 
